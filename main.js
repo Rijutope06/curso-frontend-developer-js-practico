e7086cae235515aadc87fe2b39c42515ed548e43
Ricardo Junior Torres Perez <ricardo.torres@siesacloudfe.com>
2023-03-27 13:46:47 -0500

    Se creo función que crea los elementos html con sus clases y atributos

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const ShoppingCart = document.querySelector(".navbar-shopping-cart");
 const productDetail = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector('.cards-container')
 
 menuEmial.addEventListener("click", toggleDestockMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
@@ -34,8 +35,67 @@ function toggleShoppingCart() {
   if (!isMobileMenuClosed) {
     mobileMenu.classList.add("inactive");
   }
-  
- 
 
   productDetail.classList.toggle("inactive");
 }
+
+const producList = [];
+producList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+producList.push({
+  name: "Pantalla",
+  price: 240,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+producList.push({
+  name: "Computador",
+  price: 820,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+
+function porductListRender (arr) {
+  for (product of producList) {
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+  
+    const productImg = document.createElement("img");
+    productImg.setAttribute("src", product.image);
+  
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+  
+    const productInfoDiv = document.createElement("div");
+    const productName = document.createElement("p");
+    const productPrice = document.createElement("p");
+    productPrice.innerText = "$" + product.price;
+    productName.innerText = product.name;
+  
+    productInfoDiv.appendChild(productPrice)
+    productInfoDiv.appendChild(productName)
+  
+    productInfoFigure = document.createElement("figure");
+    productImgCart = document.createElement("img");
+    productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+    
+    productInfoFigure.appendChild(productImgCart)
+  
+    productInfo.appendChild(productInfoDiv)
+    productInfo.appendChild(productInfoFigure)
+  
+    productCard.appendChild(productImg)
+    productCard.appendChild(productInfo)
+  
+    cardsContainer.appendChild( productCard)
+  }
+}
+
+porductListRender(producList)
